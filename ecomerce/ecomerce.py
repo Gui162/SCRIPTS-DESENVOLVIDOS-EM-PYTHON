a3640ccb2094bb57f86a29aeb60c6e4d46802ce7
Guilherme Scherer <126033435+Gui162@users.noreply.github.com>
2023-05-18 16:01:47 -0300

    a

--- ecomerce/ecomerce.py
+++ ecomerce/ecomerce.py
@@ -2,16 +2,16 @@ import pyautogui as py
 import pandas as pd
 from time import sleep
 from pyperclip import copy
-dados = pd.read_excel("produtos_negrao_sao_sebastiao.xlsx")
-
-codigo_float = dados["sku_ss"].iloc[0]
+dados = pd.read_excel("produtos.xlsx")
+linha = 3
+codigo_float = dados["sku_ss"].iloc[linha]
 codigo = str(codigo_float).rstrip(".0")
-nome = dados["nome_erp"].iloc[0]
-altura = str((dados["altura"].iloc[0])/100)
-comprimento = str((dados["comprimento"].iloc[0])/100)
-largura = str((dados["largura"].iloc[0])/100)
-peso = str(dados["peso"].iloc[0])
-descricao = dados["descricao"].iloc[0]
+nome = dados["nome_erp"].iloc[linha]
+altura = str((dados["altura"].iloc[linha])/100)
+comprimento = str((dados["comprimento"].iloc[linha])/100)
+largura = str((dados["largura"].iloc[linha])/100)
+peso = str(dados["peso"].iloc[linha])
+descricao = dados["descricao"].iloc[linha]
 
 
 #pesquisa do codigo
@@ -22,11 +22,11 @@ py.moveTo(x=1986, y=208)
 py.click()
 sleep(1)
 py.write(codigo)
-sleep(0.01)
+sleep(0.05)
 py.press("enter")
-sleep(0.01)
+sleep(0.05)
 py.moveTo(x=2000, y=233)
-sleep(0.01)
+sleep(0.05)
 py.doubleClick()
 py.moveTo(x=2460, y=195)
 sleep(0.6)
@@ -39,11 +39,12 @@ sleep(0.01)
 py.write(comprimento)
 sleep(0.01)
 py.press("tab")
+sleep(0.03)
 py.click()
 py.write(largura)
-sleep(0.001)
+sleep(0.03)
 py.press("tab")
-sleep(0.001)
+sleep(0.03)
 py.write(altura)
 sleep(0.01)
 
@@ -51,24 +52,25 @@ sleep(0.01)
 py.moveTo(x=2055, y=250)
 py.click()
 py.write(peso)
-sleep(0.01)
+sleep(0.03)
 py.press("tab")
-sleep(0.001)
+sleep(0.03)
 py.write(peso)
-sleep(0.02)
+sleep(0.03)
 
 #aba ecomerce
 py.moveTo(x=2800, y=195)
-sleep(0.01)
+sleep(0.03)
 py.click()
-sleep(0.02)
+sleep(0.03)
 py.moveTo(x=2350, y=342)
 py.click()
 copy(nome)
 py.hotkey('ctrl', 'v')
-sleep(0.01)
+sleep(0.03)
 py.moveTo(x=2350, y=730)
 py.click()
-sleep(0.01)
+sleep(0.03)
 py.write(descricao)
-sleep(0.01)
+sleep(0.03)
+py.hotkey('ctrl', 's')
