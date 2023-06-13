b806e71c0423357fc943bfa2752685072754eb5e
Guilherme Scherer <126033435+Gui162@users.noreply.github.com>
2023-06-13 09:59:53 -0300

    a

--- ssb/identificacao_img/au.py
+++ ssb/identificacao_img/au.py
@@ -3,20 +3,23 @@ from time import sleep
 
 #identificação de imagem 
 def position():
-    pos = pyautogui.locateCenterOnScreen("mult.jpg", confidence= 0.9) #procura a imagem na tela e faz se tiver 90% de certeza vira true se nao retorna NONE
+    pos = pyautogui.locateCenterOnScreen("identificacao_img\multi.jpg", confidence= 0.60) #procura a imagem na tela e faz se tiver 90% de certeza vira true se nao retorna NONE
     if pos != None:
         print("encontrou multiplo")
+        pyautogui.press("enter")
         sleep(0.1)
         return True
 #se nao achar a imagem continua
     else:
         print("sem multiplo")
         pyautogui.write("1")
+        sleep(0.01)
         pyautogui.press("down")
+        sleep(0.02)
     return False
 def loop():
     while True:
         position()
 
-
-loop()
+sleep(5)
+loop()111111111
